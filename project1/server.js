efa785bcccd325ed4cf7e7b53c37368ca9162dbc
William Grisaitis <wgrisaitis@gmail.com>
2015-07-25 15:38:19 -0400

    adding console.log prints, URL parsing

--- project1/server.js
+++ project1/server.js
@@ -1,11 +1,21 @@
 var http = require("http");
+var url = require("url");
+var utils = require("./utils.js")
 
 function start() {
 	function onRequest(request, response) {
 		console.log("request received");
+ 		// console.log(objToString(url.parse(request.url)))
+		// console.log(typeof request.url); // prints "string"
+		var pathname = url.parse(request.url).pathname; 
+		console.log(pathname); // prints "/"" or "/favicon.ico"
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write("hello world!");
 		response.end();
+		// console.log(request.body); // prints "undefined"
+		console.log(utils.objToString(request));
+		// console.log(objToString(request.headers));
+		// console.log(request.headers['user-agent']);
 		//console.log("print to console..."); 
 		// this prints 3 times... why?
 		// --> chrome issues multiple requests
@@ -16,3 +26,4 @@ function start() {
 }
 
 exports.start = start; // makes start() available elsewhere
+exports.test = "whoa";
