5781ba3b98426ecc195aa9648c9a144912882bd4
William Grisaitis <wgrisaitis@gmail.com>
2015-07-25 14:32:04 -0400

    creating call back function separately

--- project1/server.js
+++ project1/server.js
@@ -1,13 +1,18 @@
 var http = require("http");
 
+function onRequest(request, response) {
+	console.log("request received");
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("hello world!");
+	response.end();
+//	console.log("print to console..."); // this prints 3 times... why?
+}
+
 var server = http.createServer(
-	function(request, response) {
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Hello World");
-		response.end();
-		console.log("print to console..."); // this prints 3 times... why?
-	}
+	onRequest
 );
 
 server.listen(8888);
+
+console.log("server started");
 
