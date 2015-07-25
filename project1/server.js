b7c5e01d18c466dd2b157021ca7a013ce2f1e222
William Grisaitis <wgrisaitis@gmail.com>
2015-07-25 11:42:53 -0400

    creating server object

--- project1/server.js
+++ project1/server.js
@@ -1,11 +1,13 @@
 var http = require("http");
 
-http.createServer(
+var server = http.createServer(
 	function(request, response) {
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write("Hello World");
 		response.end();
 		console.log("print to console..."); // this prints 3 times... why?
 	}
-).listen(8888);
+);
+
+server.listen(8888);
 
