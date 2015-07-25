0ba021c2372f523f5f98d1845da14448e48f5fe6
William Grisaitis <wgrisaitis@gmail.com>
2015-07-25 11:39:26 -0400

    adding console.log() for testing

--- project1/server.js
+++ project1/server.js
@@ -5,6 +5,7 @@ http.createServer(
 		response.writeHead(200, {"Content-Type": "text/plain"});
 		response.write("Hello World");
 		response.end();
+		console.log("print to console..."); // this prints 3 times... why?
 	}
 ).listen(8888);
 
