318d5705e26dd66b281a49e09d8e853f20b21edf
William Grisaitis <wgrisaitis@gmail.com>
2015-07-25 14:42:10 -0400

    shifting code to a function to be exported

--- project1/server.js
+++ project1/server.js
@@ -1,20 +1,18 @@
 var http = require("http");
 
-function onRequest(request, response) {
-	console.log("request received");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("hello world!");
-	response.end();
-//	console.log("print to console..."); 
-	// this prints 3 times... why?
-	// because chrome issues multiple requests
+function start() {
+	function onRequest(request, response) {
+		console.log("request received");
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("hello world!");
+		response.end();
+		//console.log("print to console..."); 
+		// this prints 3 times... why?
+		// --> chrome issues multiple requests
+	}
+	var server = http.createServer(onRequest);
+	server.listen(8888);
+	console.log("server started");
 }
 
-var server = http.createServer(
-	onRequest
-);
-
-server.listen(8888);
-
-console.log("server started");
-
+exports.start = start; // makes start() available elsewhere
